4dea366daf69e0e48d5be93917cc05400d6f716b
chris <chri402s@stud.kea.dk>
2024-09-20 14:56:27 +0200

    seperated list items from layout

--- frontend/src/components/ListLayout.tsx
+++ frontend/src/components/ListLayout.tsx
@@ -1,29 +1,9 @@
-import { Button, VStack, HStack, Box } from "@chakra-ui/react";
+import { Button, VStack, Box } from "@chakra-ui/react";
 import { useState} from "react";
+import ListItem from "./Listitems";
 
 type Item = {
     id: number;
-};
-
-const ListItems = ({ id }: { id:number }) => {
-    return (
-        <HStack
-            justifyContent="space-between"
-            w="100%"
-            bg="gray.600"
-            p={4}
-            borderRadius="md"
-        >
-            <Box flex="1" color="white">
-                Name {id}
-            </Box>
-            <HStack spacing={4}>
-                <Button colorScheme="blue">Select</Button>
-                <Button colorScheme="gray">Edit</Button>
-                <Button colorScheme="red">Delete</Button>
-            </HStack>
-        </HStack>
-    );
 };
 
 const ListLayout = () => {
@@ -39,10 +19,10 @@ const ListLayout = () => {
     return (
         <VStack spacing={4} p={6} bg="gray.300" align="stretch">
             {items.map((item) => (
-                <ListItems key={item.id} id={item.id} />
+                <ListItem key={item.id} id={item.id} />
             ))}
 
-            <Box display="flex" justifyContent="flex-end" p={4}>
+            <Box display="flex" justifyContent="flex-end" p={5}>
                 <Button colorScheme="green" onClick={handleAddItem}>New</Button>
             </Box>
         </VStack>
