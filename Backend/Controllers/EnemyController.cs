2d291c361ce6eb46c8e6e305a8328f79df1aeb8b
victor-dat21b <vict702k@stud.kea.dk>
2024-09-21 11:06:10 +0200

    Enemy crud made. Still missing tests

---
+++ Backend/Controllers/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using Backend.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace Backend.Controllers
+{
+    public static class EnemyController
+    {
+        public static void MapEnemyEndpoint(this WebApplication app)
+        {
+            //Get all enemies
+            app.MapGet("/enemies", (IEnemyService enemyService) =>
+            {
+                return enemyService.GetAllEnemies();
+            });
+
+            //Get enemy by id
+            app.MapGet("/enemies/{id}", (IEnemyService enemyService, int id) =>
+            {
+                return enemyService.GetEnemyById(id);
+            });
+
+            //Delete enemy
+            app.MapDelete("/enemies/{id}", (IEnemyService enemyService, int id) =>
+            {
+                enemyService.DeleteEnemy(id);
+                return Results.NoContent();
+            });
+
+            //Create enemy
+            app.MapPost("/enemies", (IEnemyService enemyService, Enemy enemy) =>
+            {
+                enemyService.CreateEnemy(enemy);
+                return Results.Created($"/enemies/{enemy.Id}", enemy);
+            });
+
+            //Update enemy
+            app.MapPut("/enemies", (IEnemyService enemyService, Enemy enemy) => {
+
+                var dbEnemy = enemyService.GetEnemyById(enemy.Id);
+
+                if (dbEnemy == null)
+                {
+                    return Results.NotFound();
+                }
+
+                enemyService.UpdateEnemy(enemy);
+                return Results.Ok(enemy);
+            });
+        }
+    }
+}
+
