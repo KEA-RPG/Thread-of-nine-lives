27078ab4ec92154855b7dff2641201e2d4a0019c
victor-dat21b <vict702k@stud.kea.dk>
2024-09-27 14:16:40 +0200

    Test changes for infrastructure

--- Infrastructure/Persistance/Relational/RelationalContext.cs
+++ Infrastructure/Persistance/Relational/RelationalContext.cs
@@ -1,16 +1,14 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Persistance.Relational
 {
     public class RelationalContext : DbContext
     {
-        public RelationalContext(DbContextOptions options):base (options) { }
+        public RelationalContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Card> Cards { get; set; }
         public DbSet<User> Users { get; set; }
     }
