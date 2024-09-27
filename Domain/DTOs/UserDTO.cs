7e304ccb0b54a7a863946c8d99a2a520594ae952
victor-dat21b <vict702k@stud.kea.dk>
2024-09-27 16:58:15 +0200

    Removed comment and added ID to UserDTO

--- Domain/DTOs/UserDTO.cs
+++ Domain/DTOs/UserDTO.cs
@@ -8,6 +8,7 @@ namespace Domain.DTOs
 {
     public class UserDTO
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
