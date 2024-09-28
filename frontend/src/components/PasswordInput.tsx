ad9c4bb45a1d7bb3fef28fec5b787912d5424bcb
chris <chri402s@stud.kea.dk>
2024-09-28 14:07:44 +0200

    changed Username/Passwordinput component to take value and onchange with a type

--- frontend/src/components/PasswordInput.tsx
+++ frontend/src/components/PasswordInput.tsx
@@ -1,9 +1,17 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
 
-const PasswordInput = () =>
-    <FormControl>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="Password" />
-    </FormControl>
+import { ChangeEvent } from 'react';
+
+interface PasswordInputProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const PasswordInput = ({ value, onChange }: PasswordInputProps) => (
+  <FormControl>
+    <FormLabel>Password</FormLabel>
+    <Input type="password" placeholder="Password" value={value} onChange={onChange} />
+  </FormControl>
+);
 
 export default PasswordInput;
--- frontend/src/components/UsernameInput.tsx
+++ frontend/src/components/UsernameInput.tsx
@@ -1,9 +1,17 @@
 import { FormControl, FormLabel, Input } from '@chakra-ui/react'
 
-const UsernameInput = () =>
+import { ChangeEvent } from 'react';
+
+interface UsernameInputProps {
+    value: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const UsernameInput = ({ value, onChange }: UsernameInputProps) => (
     <FormControl>
         <FormLabel>Username</FormLabel>
-        <Input type="text" placeholder="Username" />
+        <Input type="text" placeholder="Username" value={value} onChange={onChange} />
     </FormControl>
+);
 
 export default UsernameInput;
