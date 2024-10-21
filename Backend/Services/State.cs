ce326978962f405f052368eb86c9f014d0169d3b
chris <chri402s@stud.kea.dk>
2024-10-21 08:58:42 +0200

    added state class

---
+++ Backend/Services/State.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Backend.Services
+{
+    public class State
+    {
+        public Player Player { get; set; }
+        public Boss Boss { get; set; }
+        public List<Action> Actions { get; set; }
+
+        public State(Player player, Boss boss)
+        {
+            Actions = new List<Action>();
+            Player = player;
+            Boss = boss;
+        }
+        public void ProcessAction(Action action)
+        {
+            Actions.Add(action);
+            UpdateGameState(action);
+        }
+
+        public void UpdateGameState(Action action)
+        {
+            if (action.Type == "ATTACK")
+            {
+                Boss.Health -= action.Value;
+                if (Boss.Health <= 0)
+                {
+                    Boss.Health = 0;
+                }
+            }
+            
+        }
+
+    }
+
+    public class Player
+    {
+        public int Health { get; set; }
+        
+    }
+
+    public class Boss
+    {
+        public int Health { get; set; }
+    }
+
+    public class Action
+    {
+        public string Type { get; set; }
+        public int Value { get; set; }
+    }
+}
