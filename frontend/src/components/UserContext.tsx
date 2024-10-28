7fe3ee49763accbf83128d95adef52a4340aff21
Bjand49 <bjand49@gmail.com>
2024-10-28 18:41:28 +0100

    Frontend login (#20)
    Added login and logout features to the frontend. 
    
    Also implemented controls to apply user restrictions on routes.
    
    Re-wrote the existing hooks to transfer the token on transmission

--- frontend/src/components/UserContext.tsx
+++ frontend/src/components/UserContext.tsx
@@ -1,31 +1,102 @@
-import React, { createContext, useState, ReactNode } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-interface User {
-  loggedIn: boolean;
-  isAdmin: boolean;
-}
+import { jwtDecode } from 'jwt-decode';
+import { Response } from '../services/apiClient';
+import { Credentials, Token, useLogin, useSignUp } from "../hooks/useUser";
 
 interface UserContextType {
-  user: User | null;
-  loginAsUser: () => void;
-  loginAsAdmin: () => void;
+  token?: string;
+  username?: string;
+  login: (credentials: Credentials) => Promise<Response<Token>>;
   logout: () => void;
+  signUp: (credentials: Credentials) => Promise<Response<string>>;
+  requireLogin: (role: string) => void;
+  role?: string;
+}
+interface JwtToken {
+  sub: string;
+  iat: number;
+  exp: number;
+  iss?: string;
+  aud?: string | string[];
+  role?: string;
+  name: string;
 }
 
+// Create the UserContext
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
-const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+// UserContext provider component
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [role, setRole] = useState<string>();
+  const [token, setToken] = useState<string>(localStorage.getItem("token") || "");
+  const [username, setUsername] = useState<string>();
+  const navigate = useNavigate();
 
-  const loginAsUser = () => setUser({ loggedIn: true, isAdmin: false });
-  const loginAsAdmin = () => setUser({ loggedIn: true, isAdmin: true });
-  const logout = () => setUser(null);
+  useEffect(() => {
+    if (token) {
+      const decodedToken = jwtDecode<JwtToken>(token);
+      setRole(decodedToken.role?.toLowerCase());
+      setUsername(decodedToken.name);
+    }
+  }, [token]);
 
+  const login = async (credentials: Credentials): Promise<Response<Token>> => {
+    const loggedInUser = await useLogin(credentials);
+
+    if (loggedInUser && loggedInUser.data) {
+      setToken(loggedInUser.data.token)
+      const decodedToken = jwtDecode(loggedInUser.data.token) as JwtToken;
+      if (decodedToken.role) {
+        localStorage.setItem("token", loggedInUser.data.token);
+        setRole(decodedToken.role.toLowerCase());
+        setUsername(decodedToken.name);
+        navigate('/menu');
+      }
+    }
+    else {
+      console.error('Login failed: result is undefined');
+    }
+    return loggedInUser;
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setRole("");
+    setUsername("");
+    navigate('/');
+  };
+
+  const signUp = async (credentials: Credentials): Promise<Response<string>> => {
+    const signedUpUser = await useSignUp(credentials);
+    return signedUpUser;
+  }
+  const requireLogin = (requiredRole: string) => {
+    if (!token) {
+      console.error('No token found, redirecting to login');
+      navigate('/login');
+    }
+    else if (requiredRole === "admin" && role !== "admin") {
+      console.error('Admin role required, but current role is not admin, redirecting to login');
+      navigate('/login');
+    }
+    else if (requiredRole !== "player" && requiredRole !== "admin") {
+      console.error('Invalid role specified, role not found');
+    }
+  }
   return (
-    <UserContext.Provider value={{ user, loginAsUser, loginAsAdmin, logout }}>
+    <UserContext.Provider value={{ token, username, login, logout, signUp, requireLogin, role }}>
       {children}
     </UserContext.Provider>
   );
 };
-
-export { UserProvider, UserContext };
+// Custom hook to use the UserContext
+export const useUserContext = (): UserContextType => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUserContext must be used within a UserProvider');
+  }
+  return context;
+};
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,58 @@
+import { Image, Button, Card, CardBody, CardFooter, Stack, Text, Link, Box } from '@chakra-ui/react';
+import { useState } from 'react';
+import InputFieldElement from '../components/InputFieldElement';
+import { Credentials } from '../hooks/useUser';
+import { useUserContext } from '../components/UserContext'; // Assuming you're handling auth here
+
+const Login = () => {
+  const [credentials, setCredentials] = useState<Credentials>({ username: '', password: '' });
+  const [error, setError] = useState<boolean>(false);
+  const { login } = useUserContext();
+
+  const handleLogin = async () => {
+    setError(false);
+    const test = await login(credentials);
+    if (test.error) {
+      setError(true);
+    };
+  }
+
+  return (
+    <Card direction={{ base: 'column', sm: 'row' }} overflow="hidden" variant="elevated">
+      <Image objectFit="cover" maxW={{ base: '100%', sm: '200px' }} src="https://loremflickr.com/1280/720" />
+
+      <Stack>
+        <CardBody>
+          <InputFieldElement
+            type="text"
+            name="Username"
+            placeholder="Username"
+            onChange={(username) => setCredentials({ ...credentials, username })}
+          />
+          <InputFieldElement
+            type="password"
+            name="Password"
+            placeholder="Password"
+            onChange={(password) => setCredentials({ ...credentials, password: password })}
+          />
+          <Button variant="solid" colorScheme="blue" mt={3} onClick={handleLogin} >
+            Sign in
+          </Button>
+          <Box h={2}>
+            {error && <Text color="red.500">
+              Invalid username or password
+            </Text>}
+          </Box>
+        </CardBody>
+
+        <CardFooter>
+          <Text>
+            Or sign up <Link href="/signup">here</Link>
+          </Text>
+        </CardFooter>
+      </Stack>
+    </Card>
+  );
+};
+
+export default Login;
