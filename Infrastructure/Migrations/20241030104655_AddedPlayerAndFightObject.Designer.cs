27246447c75a66b4549461602ff3818d5a3e07e4
chris <chri402s@stud.kea.dk>
2024-10-30 12:59:01 +0100

    added player and fight in migrations

--- Infrastructure/Migrations/20241030104655_AddedPlayerAndFightObject.Designer.cs
+++ Infrastructure/Migrations/20241030104655_AddedPlayerAndFightObject.Designer.cs
@@ -3,6 +3,7 @@ using Infrastructure.Persistance.Relational;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(RelationalContext))]
-    partial class RelationalContextModelSnapshot : ModelSnapshot
+    [Migration("20241030104655_AddedPlayerAndFightObject")]
+    partial class AddedPlayerAndFightObject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -52,7 +55,7 @@ namespace Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Cards", (string)null);
+                    b.ToTable("Cards");
                 });
 
             modelBuilder.Entity("Domain.Entities.Enemy", b =>
@@ -76,7 +79,7 @@ namespace Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Enemies", (string)null);
+                    b.ToTable("Enemies");
                 });
 
             modelBuilder.Entity("Domain.Entities.Fight", b =>
@@ -93,7 +96,7 @@ namespace Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Fights", (string)null);
+                    b.ToTable("Fights");
                 });
 
             modelBuilder.Entity("Domain.Entities.Player", b =>
@@ -109,7 +112,7 @@ namespace Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Players", (string)null);
+                    b.ToTable("Players");
                 });
 
             modelBuilder.Entity("Domain.Entities.User", b =>
@@ -134,7 +137,7 @@ namespace Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Users", (string)null);
+                    b.ToTable("Users");
                 });
 #pragma warning restore 612, 618
         }
