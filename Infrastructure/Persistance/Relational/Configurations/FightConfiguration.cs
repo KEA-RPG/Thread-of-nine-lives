5028dc3f777fcf882f10889a91c89cac829aa625
chris <chri402s@stud.kea.dk>
2024-11-17 13:07:22 +0100

    added fight Configurations, maybe delete after migrations implemented

---
+++ Infrastructure/Persistance/Relational/Configurations/FightConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Relational.Configurations
+{
+    internal class FightConfiguration : IEntityTypeConfiguration<Fight>
+    {
+        public void Configure(EntityTypeBuilder<Fight> builder)
+        {
+            // Set primary key
+            builder.HasKey(f => f.Id);
+
+            // Configure relationship with User entity
+            builder.HasOne(f => f.User)
+                   .WithMany(u => u.Fights)
+                   .HasForeignKey(f => f.UserId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            // Configure relationship with Enemy entity
+            builder.HasOne(f => f.Enemy)
+                   .WithMany(e => e.Fights)
+                   .HasForeignKey(f => f.EnemyId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
