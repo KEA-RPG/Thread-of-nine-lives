bbfe4c55980330117774ff316f9e171346e8957b
victor-dat21b <91475975+victor-dat21b@users.noreply.github.com>
2024-11-17 13:54:00 +0100

    Made indexes for Decks, Comments and User" (#33)
    Made Index for Comments when it searches for DeckId. Made Index for User when it searches for Username.
    Made index for Deck when it searches for either is_public or user_id.
    Also made some changes to a User database fields. So they are not vchar(max) but reduced to vchar(255) or similar.

---
+++ Infrastructure/Persistance/Relational/Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Relational.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            // Set properties
+            builder.Property(u => u.Username)
+                   .HasMaxLength(255)
+                   .IsRequired();
+
+            builder.Property(u => u.PasswordHash)
+                   .HasMaxLength(512)
+                   .IsRequired();
+
+            builder.Property(u => u.Role)
+                   .HasMaxLength(255)
+                   .IsRequired();
+
+            // Create index on Username with included columns
+            builder.HasIndex(u => u.Username)
+                   .HasDatabaseName("idx_username")
+                   .IncludeProperties(u => new { u.PasswordHash, u.Role });
+        }
+    }
+}
