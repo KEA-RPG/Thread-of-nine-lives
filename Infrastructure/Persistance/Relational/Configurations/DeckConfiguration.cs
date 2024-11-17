2ff53499e243374ffcb251bed23249418ff246c7
victor-dat21b <vict702k@stud.kea.dk>
2024-11-17 13:46:59 +0100

    Made indexes for Decks, Comments and User"

--- Infrastructure/Persistance/Relational/Configurations/DeckConfiguration.cs
+++ Infrastructure/Persistance/Relational/Configurations/DeckConfiguration.cs
@@ -11,7 +11,17 @@ namespace Infrastructure.Persistance.Relational.Configurations
             builder.Property(b => b.IsPublic).HasDefaultValue(false);
 
 
-                   
+            // Create index on UserId with included columns
+            builder.HasIndex(d => d.UserId)
+                   .HasDatabaseName("idx_user_id")
+                   .IncludeProperties(d => new { d.Name, d.IsPublic });
+
+            // Create index on IsPublic with included columns
+            builder.HasIndex(d => d.IsPublic)
+                   .HasDatabaseName("idx_is_public")
+                   .IncludeProperties(d => new { d.UserId, d.Name });
+
+
         }
     }
 }
