4cc6a2eb112ae514b53d379271b0ad8d349dc6e8
Troels-dat21B <troe1912@stud.kea.dk>
2024-11-20 15:20:49 +0100

    Changed DeckCard compositekey to a single primary key. Added migration for it aswell

--- Infrastructure/Migrations/20241120141152_Changed DeckCard composite key to one primary key.Designer.cs
+++ Infrastructure/Migrations/20241120141152_Changed DeckCard composite key to one primary key.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistance.Relational;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(RelationalContext))]
-    partial class RelationalContextModelSnapshot : ModelSnapshot
+    [Migration("20241120141152_Changed DeckCard composite key to one primary key")]
+    partial class ChangedDeckCardcompositekeytooneprimarykey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/20241120141152_Changed DeckCard composite key to one primary key.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedDeckCardcompositekeytooneprimarykey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DeckCards",
+                table: "DeckCards");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "DeckCards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DeckCards",
+                table: "DeckCards",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeckCards_DeckId",
+                table: "DeckCards",
+                column: "DeckId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DeckCards",
+                table: "DeckCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DeckCards_DeckId",
+                table: "DeckCards");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "DeckCards");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DeckCards",
+                table: "DeckCards",
+                columns: new[] { "DeckId", "CardId" });
+        }
+    }
+}
