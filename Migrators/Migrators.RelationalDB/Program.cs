43bb6bddfc3acfcb8a85e6ebc3ac61da0fd4053f
victor-dat21b <vict702k@stud.kea.dk>
2024-11-20 15:06:20 +0100

    Migrator now works with correct users

--- Migrators/Migrators.RelationalDB/Program.cs
+++ Migrators/Migrators.RelationalDB/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Reflection;
 
-
 var binpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
 var configuration = new ConfigurationBuilder()
@@ -16,14 +15,30 @@ var configuration = new ConfigurationBuilder()
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 var builder = Host.CreateDefaultBuilder(args)
-.ConfigureServices(services =>
-{
-
-    services.AddDbContext<RelationalContext>(options =>
+    .ConfigureServices(services =>
     {
-        options.UseSqlServer(connectionString,
-        b => b.MigrationsAssembly("Infrastructure"));
+        services.AddDbContext<RelationalContext>(options =>
+        {
+            options.UseSqlServer(connectionString,
+                b => b.MigrationsAssembly("Infrastructure"));
+        });
     });
 
+using (var host = builder.Build())
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
 
-});
+        try
+        {
+            var context = services.GetRequiredService<RelationalContext>();
+            context.Database.Migrate();  // Apply pending migrations
+            Console.WriteLine("Database migration completed successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while migrating the database: {ex.Message}");
+        }
+    }
+}
