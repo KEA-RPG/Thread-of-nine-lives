f42c3cf70377bf8eb304b6c83abfce66cfed673e
victor-dat21b <vict702k@stud.kea.dk>
2024-11-20 11:16:16 +0100

    Created new project that should handle database setup, but doesnt work yet"

---
+++ Migrators/Migrators.RelationalDB/Program.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Persistance.Relational;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+
+var binpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(binpath)
+    .AddJsonFile("dbsettingsrelational.json")
+    .Build();
+
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+var builder = Host.CreateDefaultBuilder(args)
+.ConfigureServices(services =>
+{
+
+    services.AddDbContext<RelationalContext>(options =>
+    {
+        options.UseSqlServer(connectionString,
+        b => b.MigrationsAssembly("Infrastructure"));
+    });
+
+
+});
