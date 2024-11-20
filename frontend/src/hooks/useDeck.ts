7fbb1eda3167526ef07b426be5948828f7184184
Bjørn <bjand49@gmail.com>
2024-11-20 11:31:18 +0100

    Created usedeck hooks

---
+++ frontend/src/hooks/useDeck.ts
@@ -0,0 +1,29 @@
+import ApiClient from "../services/apiClient";
+
+export interface Deck {
+    id?: number;
+    name: string;
+    userId: number;
+    Decks: Deck[];
+    isPublic: boolean;
+    comments: Comment[];
+}
+
+export interface Comment {
+    id?: number;
+    deckId: number;
+    text: string;
+    createdAt: Date;
+}
+const apiClient = new ApiClient();
+
+const useUserDecks = () => apiClient.get<Deck[]>(`/decks`);
+const usePublicDecks = () => apiClient.get<Deck[]>(`/decks/public`)
+const useDeck = (id: number) => apiClient.get<Deck>(`/decks/${id}`);
+const useDeckComments = (id: number) => apiClient.get<Comment[]>(`/decks/${id}/comments`);
+const postComment = (comment: Comment, id: number) => apiClient.post<Comment, Deck>(`/decks/${id}/comments`, comment);
+const postDeck = (Deck: Deck) => apiClient.post<Deck, Deck>(`/decks`, Deck);
+const deleteDeck = (id: number) => apiClient.delete<Deck>(`/decks/${id}`);
+const updateDeck = (id: number, Deck: Deck) => apiClient.put<Deck, Deck>(`/decks/${id}`, Deck);
+
+export { useUserDecks, usePublicDecks, useDeck, useDeckComments, postComment, deleteDeck, postDeck, updateDeck }
