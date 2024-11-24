c00ae9ee1d52590083447cb08885183ea8eccb87
bjørn <bjand49@gmail.com>
2024-11-24 15:41:58 +0100

    Start on integration tests

--- Backend/Repositories/Document/MongoCardRepository.cs
+++ Backend/Repositories/Document/MongoCardRepository.cs
@@ -28,6 +28,18 @@ namespace Backend.Repositories.Document
         public void DeleteCard(CardDTO card)
         {
             _context.Cards().DeleteOne(x => x.Id == card.Id);
+
+            var decksFilter = Builders<DeckDTO>.Filter.ElemMatch(d => d.Cards, c => c.Id == card.Id);
+            var affectedDecks = _context.Decks().Find(decksFilter).ToList();
+
+            foreach (var deck in affectedDecks)
+            {
+                deck.Cards = deck.Cards.Where(c => c.Id != card.Id).ToList();
+
+                // Update the Deck in the Decks collection
+                var deckUpdateFilter = Builders<DeckDTO>.Filter.Eq(d => d.Id, deck.Id);
+                _context.Decks().ReplaceOne(deckUpdateFilter, deck);
+            }
         }
 
         public void UpdateCard(CardDTO card)
@@ -35,7 +47,19 @@ namespace Backend.Repositories.Document
             var filter = Builders<CardDTO>.Filter.Eq(c => c.Id, card.Id);
             var update = Builders<CardDTO>.Update.Set(c => c, card);
 
-            _context.Cards().UpdateOne(filter, update);
+            _context.Cards().ReplaceOne(filter, card);
+
+            var decksFilter = Builders<DeckDTO>.Filter.ElemMatch(d => d.Cards, c => c.Id == card.Id);
+            var affectedDecks = _context.Decks().Find(decksFilter).ToList();
+            
+            foreach (var deck in affectedDecks)
+            {
+                var cardToUpdate = deck.Cards.First(c => c.Id == card.Id);
+                cardToUpdate = card;
+                
+                var deckUpdateFilter = Builders<DeckDTO>.Filter.Eq(d => d.Id, deck.Id);
+                _context.Decks().ReplaceOne(deckUpdateFilter, deck);
+            }
         }
 
         public List<CardDTO> GetAllCards()
