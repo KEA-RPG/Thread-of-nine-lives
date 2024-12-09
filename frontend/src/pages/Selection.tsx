179155d025cc2ce60d309cf0044706471906a3d8
chris <chri402s@stud.kea.dk>
2024-12-09 17:51:30 +0100

    made select button initial game instead of extra button

--- frontend/src/pages/Selection.tsx
+++ frontend/src/pages/Selection.tsx
@@ -1,40 +1,27 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { initGame, StateGameInit } from '../hooks/useGame';
-import { useToast, Spinner, Button, Text } from '@chakra-ui/react';
+import { useToast, Spinner, Text } from '@chakra-ui/react';
 import { useEnemies, Enemy } from '../hooks/useEnemy';
 import ListLayout from '../components/ListLayout';
 
 const SelectionPage = () => {
-    const [selectedEnemy, setSelectedEnemy] = useState<Enemy | null>(null);
     const [enemies, setEnemies] = useState<Enemy[]>([]);
     const { data, error } = useEnemies();
     const navigate = useNavigate();
     const toast = useToast()
 
-    const onSelection = (item: Enemy) => {
-        setSelectedEnemy(item);
-    };
-
-    const initializeGameState = async () => {
-        if (!selectedEnemy) {
-            toast({
-                description: 'Please select an enemy',
-                status: 'warning',
-            });
-            return;
-        }
-
-        const { data, error } = await initGame({ enemyId: selectedEnemy.id! } as StateGameInit);
+    const onSelection = async (item: Enemy) => {
+        const { data, error } = await initGame({ enemyId: item.id! } as StateGameInit);
         if (!data || error) {
             toast({
-                description: 'Error: Failed to create combat',
+                description: 'Failed to start the game',
                 status: 'error',
             });
             return;
         }
 
-        navigate('/combat/' + data.fightId);
+        navigate(`/combat/${data.fightId}`);
     };
 
     useEffect(() => {
@@ -58,20 +45,8 @@ const SelectionPage = () => {
             </Text>
             <ListLayout
                 data={enemies}
-                onSelection={(item: Enemy) => onSelection(item)}
+                onSelection={(item: Enemy) => onSelection(item)}    
             />
-            <Button
-                onClick={initializeGameState}
-                disabled={!selectedEnemy}
-                colorScheme="teal"
-                size="lg"
-                mt={4}
-                w="50%"
-                _hover={{ bg: "teal.600" }}
-                _disabled={{ bg: "gray.400", cursor: "not-allowed" }}
-            >
-                Start Game
-            </Button>
         </div>
     );
 };
