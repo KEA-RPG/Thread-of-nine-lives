a40bb904fa4537063f21bee6f245097acad6a3fb
Troels-dat21B <troe1912@dat.stud.kea>
2024-12-10 18:19:37 +0100

    fixed missing curly bracket

--- Migrators/Migrators.RelationalDB/Program.cs
+++ Migrators/Migrators.RelationalDB/Program.cs
@@ -30,34 +30,44 @@ namespace Migrators.RelationalDB
                     });
                 });
 
-using (var host = builder.Build())
-{
-    using (var scope = host.Services.CreateScope())
-    {
-        var services = scope.ServiceProvider;
-        var context = services.GetRequiredService<RelationalContext>();
 
-        //Tager navnene på migrations for debugging
-        var pendingMigrations = context.Database.GetPendingMigrations().ToList();
 
-        if (pendingMigrations.Any())
-        {
-            var migrationsNames = string.Join(", ", pendingMigrations);
 
-            Console.WriteLine($"Attempting to do database migration on {migrationsNames}");
-        }
-        else
-        {
-            Console.WriteLine("No pending migrations found.");
-        }
-        try
-        {
-            context.Database.Migrate();  // Apply pending migrations
-            Console.WriteLine("Database migration completed successfully.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred while migrating the database: {ex.Message}");
+            using (var host = builder.Build())
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var context = services.GetRequiredService<RelationalContext>();
+
+                    //Tager navnene på migrations for debugging
+                    var pendingMigrations = context.Database.GetPendingMigrations().ToList();
+
+                    if (pendingMigrations.Any())
+                    {
+                        var migrationsNames = string.Join(", ", pendingMigrations);
+
+                        Console.WriteLine($"Attempting to do database migration on {migrationsNames}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No pending migrations found.");
+                    }
+                    try
+                    {
+                        context.Database.Migrate();  // Apply pending migrations
+                        Console.WriteLine("Database migration completed successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"An error occurred while migrating the database: {ex.Message}");
+
+                    }
+                }
+            }
         }
     }
 }
+    
+
+
