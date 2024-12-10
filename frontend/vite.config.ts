767f9d03c128a2fe958dbee6daa71c50bd3d42e7
bjørn <bjand49@gmail.com>
2024-12-10 15:28:44 +0100

    remove https on frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: { https: true }, // Not needed for Vite 5+
-  plugins: [ mkcert(),react() ]
+  
+  plugins: [react() ]
 })
