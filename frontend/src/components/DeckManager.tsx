8ae5386eb66a06856bf43e7978461797dadbf043
Bjand49 <bjand49@gmail.com>
2024-12-16 11:20:47 +0100

    cleanup in deckmanager, and bugfix (#63)

--- frontend/src/components/DeckManager.tsx
+++ frontend/src/components/DeckManager.tsx
@@ -17,138 +17,161 @@ interface Props {
 const DeckManager = ({ deckModel }: Props) => {
     const { data } = useCards();
     const navigate = useNavigate();
-    const toast = useToast()
+    const toast = useToast();
+    const [condensedDeckState, setCondensedDeckState] = useState<{ [key: number]: CardNumber }>({});
+
+    const [cards, setCards] = useState<Card[]>([]);
+    const [deck, setDeck] = useState<Deck>(
+        deckModel ?? {
+            name: "",
+            cards: [],
+            isPublic: false,
+            comments: [],
+            userId: 0,
+        }
+    );
 
     useEffect(() => {
         if (data !== undefined) {
-            setCards(data)
+            setCards(data);
         }
     }, [data]);
 
-    const [cards, setCards] = useState<Card[]>([]);
-    const [deck, setDeck] = useState<Deck>(deckModel ?? {
-        name: "",
-        cards: [],
-        isPublic: false,
-        comments: [],
-        userId: 0
-    });
 
-    const condensedDeck: { [key: number]: CardNumber } = {};
-    const getGroupedDeck = () => {
+
+
+    useEffect(() => {
+        const updatedCondensedDeck: { [key: number]: CardNumber } = {};
         for (const element of deck.cards) {
-            if (condensedDeck[element.id!] === undefined) {
-                condensedDeck[element.id!] = { card: element, count: 1 };
+            if (updatedCondensedDeck[element.id!] === undefined) {
+                updatedCondensedDeck[element.id!] = { card: element, count: 1 };
             } else {
-                condensedDeck[element.id!].count++;
+                updatedCondensedDeck[element.id!].count++;
             }
         }
+        setCondensedDeckState(updatedCondensedDeck);
+    }, [deck]); 
+
+    const getGroupedDeck = () => {
         return (
             <>
-                {Object.keys(condensedDeck).length === 0 ? (
+                {Object.keys(condensedDeckState).length === 0 ? (
                     <Heading>No cards added</Heading>
                 ) : (
-                    <SimpleGrid gap="10px" templateRows="repeat(auto-fill, 20px)" w="100%" p="10px" >
-                        {Object.entries(condensedDeck).map(([key, value]) => (
-                            <VStack key={key} onClick={() => removeCard(value.card)} w="100%" cursor="pointer" h="25px" >
-
+                    <SimpleGrid gap="10px" templateRows="repeat(auto-fill, 20px)" w="100%" p="10px">
+                        {Object.entries(condensedDeckState).map(([key, value]) => (
+                            <VStack key={key} onClick={() => removeCard(value.card)} w="100%" cursor="pointer" h="25px">
                                 <SelectedCardListItem count={value.count} title={value.card.name} />
                             </VStack>
                         ))}
                     </SimpleGrid>
-
                 )}
             </>
         );
-
     };
-
 
     const addCard = (card: Card) => {
-        setDeck(prevDeck => ({
+        setDeck((prevDeck) => ({
             ...prevDeck,
-            cards: [...prevDeck.cards, card]
+            cards: [...prevDeck.cards, card],
         }));
     };
+
     const handleUpsert = async () => {
         try {
             const isNewDeck = deck.id === undefined;
-            const result = isNewDeck
-                ? await postDeck(deck)
-                : await putDeck(deck.id!, deck);
+            const result = isNewDeck ? await postDeck(deck) : await putDeck(deck.id!, deck);
 
             if (result.error) {
                 toast({
-                    description: `Error: Failed to ${isNewDeck ? 'save' : 'update'} deck`,
+                    description: `Error: Failed to ${isNewDeck ? "save" : "update"} deck`,
                     status: "error",
                 });
                 return;
             }
 
             toast({
-                description: `Deck ${isNewDeck ? 'created' : 'updated'} successfully`,
+                description: `Deck ${isNewDeck ? "created" : "updated"} successfully`,
                 status: "success",
             });
 
-            navigate('/decks');
+            navigate("/decks");
         } catch (error) {
             toast({
                 description: "Error: Failed to save deck",
                 status: "error",
             });
         }
-    }
+    };
 
     const removeCard = (card: Card) => {
-        setDeck(prevDeck => {
+        setDeck((prevDeck) => {
             const cardIndex = prevDeck.cards.indexOf(card);
             if (cardIndex === -1) {
                 return prevDeck;
+            } else {
+                const newDeck = { ...prevDeck };
+                newDeck.cards.splice(cardIndex, 1);
+                return newDeck;
             }
-            else {
-                let newdeck = prevDeck;
-                newdeck.cards.splice(cardIndex, 1)
+        });
+    };
 
-                return newdeck
-
-            }
-        })
-    }
     return (
         <VStack justifyItems={"center"}>
             <HStack>
                 <Spacer />
-                <InputFieldElement type="text" placeholder="Deck name" name="Deck Name" onChange={e => setDeck(prevDeck => ({ ...prevDeck, name: e }))} value={deck.name} />
-                <Button onClick={() => handleUpsert()} colorScheme='green'>Save deck</Button>
+                <InputFieldElement
+                    type="text"
+                    placeholder="Deck name"
+                    name="Deck Name"
+                    onChange={(e) =>
+                        setDeck((prevDeck) => ({ ...prevDeck, name: e }))
+                    }
+                    value={deck.name}
+                />
+                <Button onClick={() => handleUpsert()} colorScheme="green">
+                    Save deck
+                </Button>
                 <Checkbox
-                isChecked={deck.isPublic}
-                onChange={(e) => {
-                    setDeck(prevDeck => ({ ...prevDeck, isPublic: e.target.checked }))
-                }}>Is deck public?</Checkbox>
+                    isChecked={deck.isPublic}
+                    onChange={(e) => {
+                        setDeck((prevDeck) => ({
+                            ...prevDeck,
+                            isPublic: e.target.checked,
+                        }));
+                    }}
+                >
+                    Is deck public?
+                </Checkbox>
                 <Spacer />
             </HStack>
             <HStack spacing={25} mt={10} alignItems="flex-start">
-                <Box w={{
-                    md: "450px",
-                    lg: "660px",
-                    xl: "870px"
-                }}
+                <Box
+                    w={{
+                        md: "450px",
+                        lg: "660px",
+                        xl: "870px",
+                    }}
                     overflowY="scroll"
-                    overflowX="hidden">
-                    <SimpleGrid height="70vh"
+                    overflowX="hidden"
+                >
+                    <SimpleGrid
+                        height="70vh"
                         w={{
                             md: "410px",
                             lg: "620px",
-                            xl: "830px"
+                            xl: "830px",
                         }}
                         columns={{
                             md: 2,
                             lg: 3,
-                            xl: 4
+                            xl: 4,
                         }}
                         spacing={10}
-                        p={1}>
-                        {cards.map(card => (
+                        p={1}
+                    >
+                        {cards.map((card) => (
                             <Box onClick={() => addCard(card)} key={card.id}>
                                 <CardCard card={card} />
                             </Box>
@@ -158,7 +181,7 @@ const DeckManager = ({ deckModel }: Props) => {
                 <SimpleGrid
                     w={{
                         base: "200px",
-                        xl: "300px"
+                        xl: "300px",
                     }}
                     columns={1}
                     spacing={5}
@@ -166,10 +189,17 @@ const DeckManager = ({ deckModel }: Props) => {
                     height="70vh"
                     overflowY="scroll"
                     overflowX="hidden"
-                    p={1}>
-
+                    p={1}
+                >
                     <VStack align="stretch">
-                        <Button onClick={() => setDeck(prevDeck => ({ ...prevDeck, cards: [] }))} colorScheme='red'>Clear Deck</Button>
+                        <Button
+                            onClick={() =>
+                                setDeck((prevDeck) => ({ ...prevDeck, cards: [] }))
+                            }
+                            colorScheme="red"
+                        >
+                            Clear Deck
+                        </Button>
                         {deck.cards.length === 0 ? (
                             <Heading>No cards added</Heading>
                         ) : (
